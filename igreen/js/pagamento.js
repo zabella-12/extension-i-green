97026dbc3593bc68c7651cd89b1be3fbc39772ab
zabella-12 <issaasousa@outlook.com>
2023-05-21 22:34:10 -0300

    insert site and page pay

--- igreen/js/pagamento.js
+++ igreen/js/pagamento.js
@@ -1,6 +1,5 @@
-const runTime = document.getElementById("tempCorrent");
-const carbon = document.getElementById("carbono");
-const price = document.getElementById("preco");
+const pay = document.getElementById("moneyPay");
+
 
 //const [tab] = chrome.tabs.query({active: true, currentWindow: true});
 
@@ -22,15 +21,6 @@ function updateTime() {
     var minutos = Math.round((elapsedSeconds % 3600) / 60);
     var segundos = elapsedSeconds % 60;
 
-    console.log("horas " + horas + " minutos " + (minutos/100))
-
-    // Formata o resultado no formato de horas (HH:MM:SS)
-    var resultadoFormatado = horas.toString().padStart(2, '0') + ':' +
-        minutos.toString().padStart(2, '0') + ':' +
-        segundos.toString().padStart(2, '0');
-
-    runTime.innerText = `${resultadoFormatado}`
-
 //conversor de carbonização.
 
 let gasto
@@ -42,8 +32,7 @@ if(horas <= 0){
 
 let kWh =  gasto / 1000
 let CO = 0.0817 * kWh
-carbon.innerText = `${(CO).toFixed(6)}`
-price.innerText = `${(CO * 10).toFixed(2)}`
+pay.innerText = `${(CO * 10).toFixed(2)}`
 
 // Função para limpar o tempo no localStorage
 window.addEventListener('beforeunload', function() {
